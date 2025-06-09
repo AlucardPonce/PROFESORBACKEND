9302ab24aad9dbfbbac875bad5ca649ae444151b
AlucarPonce <poncealucard@gmail.com>
2025-06-09 17:38:59 -0600

    micro DD

---
+++ BACKEND/src/main/java/mx/edu/uteq/idgs09/idgs09_01/controller/ProfesorController.java
@@ -0,0 +1,32 @@
+
+package main.java.mx.edu.uteq.idgs09.idgs09_01.controller;
+
+import org.apache.catalina.connector.Request;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.Optional;
+
+import mx.edu.uteq.idgs09.idgs09_01.model.entity.Division;
+import mx.edu.uteq.idgs09.idgs09_01.model.entity.ProgramaEducativo;
+import mx.edu.uteq.idgs09.idgs09_01.model.repository.DivisionRepo;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
+@RequestMapping("/api/division")
+public class ProfesorController {
+    
+}
